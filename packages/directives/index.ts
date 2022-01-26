53d036fff84c30edc7170e907f6166e5059a1046
Timithy <53697384+Timithy-L@users.noreply.github.com>
2022-01-26 09:03:39 +0800

    Initialize next

---
+++ packages/directives/index.ts
@@ -0,0 +1,12 @@
+import type { App } from 'vue'
+
+import { loading } from './src/loading'
+import { auth } from './src/auth'
+
+export { clickOutside } from './src/clickOutside'
+export { auth, loading }
+
+export const registerGlobalDirective = (app: App) => {
+  app.directive('loading', loading)
+  app.directive('auth', auth)
+}
